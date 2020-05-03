84b6b1adf63b58283793a4f91b3e5033a6f4e182
Andrey Fomin <andrey.n.fomin@gmail.com>
2020-05-04 01:56:12 +0700

    Exclude non-release versions in dependencies and update them

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,40 @@ plugins {
     signing
 }
 
+class VersionRejectionRule(versionPattern: String, val message: String) {
+    val versionRegex = Regex(versionPattern)
+}
+
+val versionRejectionRules = listOf(
+        VersionRejectionRule(".*\\b(m|M)\\d+\\b.*", "Milestone releases are rejected"),
+        VersionRejectionRule(".*\\b(rc|RC)\\d+\\b.*", "Release candidates are rejected"),
+        VersionRejectionRule(".*(alpha|ALPHA).*", "Alpha versions are rejected"),
+        VersionRejectionRule(".*(beta|BETA).*", "Beta versions are rejected")
+)
+
 allprojects {
     version = file("$rootDir/version.txt").readText().trim()
 
     repositories {
-        jcenter()
+        mavenCentral()
     }
 
     dependencyLocking {
         lockAllConfigurations()
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    versionRejectionRules.forEach { versionRejectionRule ->
+                        if (candidate.version.matches(versionRejectionRule.versionRegex)) {
+                            reject(versionRejectionRule.message)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     tasks.register("resolveAndLockAll") {
