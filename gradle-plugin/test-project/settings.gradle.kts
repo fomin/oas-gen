4464772a89715a10656b297b772af57f308e14ba
Andrey Fomin <andrey.n.fomin@gmail.com>
2020-09-16 23:42:39 +0700

    Add build scan support

--- gradle-plugin/test-project/settings.gradle.kts
+++ gradle-plugin/test-project/settings.gradle.kts
@@ -3,5 +3,17 @@ rootProject.name = "test-project"
 pluginManagement {
     repositories {
         mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.4"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
     }
 }
