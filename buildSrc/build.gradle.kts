e0b7438f8f743f1f759151591cb209eeeb9d9f04
Andrey Fomin <andrey.n.fomin@gmail.com>
2021-11-24 15:37:06 +0700

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    gradlePluginPortal()
+}
+
 apply(from = "src/main/kotlin/dependency-locking-conventions.gradle.kts")
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:+")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:latest.release")
+    implementation("com.github.node-gradle:gradle-node-plugin:latest.release")
+    implementation("com.gradle.publish:plugin-publish-plugin:latest.release")
 }
